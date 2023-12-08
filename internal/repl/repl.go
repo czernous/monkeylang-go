bca4cc278c8807e7358dbac21c029b5f8f550756
Anton Chernous <czernous_a@hotmail.com>
2023-12-08 13:58:47 +0000

    feat: add env  & bindings

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/czernous/monkeylang-go/internal/evaluator"
 	"github.com/czernous/monkeylang-go/internal/lexer"
+	"github.com/czernous/monkeylang-go/internal/object"
 	"github.com/czernous/monkeylang-go/internal/parser"
 )
 
@@ -28,6 +29,7 @@ const MONKEY_FACE = `
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -46,12 +48,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-        evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 
-        if evaluated != nil {
-            io.WriteString(out, evaluated.Inspect())
-            io.WriteString(out, "\n")
-        }
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 
 	}
 }
